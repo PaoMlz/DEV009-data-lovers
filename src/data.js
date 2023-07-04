5309ccd04e5cfec377dbaad82c538120ab3e84b3
Diana Morales <dianamorales100599@gmail.com>
2023-07-04 13:42:33 -0400

    FunctionSort

--- src/data.js
+++ src/data.js
@@ -1,9 +1,41 @@
 //funcion ordenar filtrar buscar y comparar
-
 export const example = () => {
   return 'example';
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+export const sort = (data, sortOrder) => {
+  data.sort((a,b)=>{
+    if (sortOrder === 'asc'){
+      return a.title < b.title;
+    }
+    else {
+      return b.title > b.title;
+    }
+  })
 };
+
+
+
+export const sortData = (data, sortBy, sortOrder) => {
+    // Verificar si data es un arreglo
+    
+  data.sort((a, b) => {
+    //  método sort() de los arrays y una función de comparación
+    // vamos a convertir los campos a ordenar a mayúsculas para asegurar una comparación correcta
+    const fieldA = a[sortBy].toUpperCase();
+    const fieldB = b[sortBy].toUpperCase();
+
+    if (fieldA < fieldB) {
+      return sortOrder === 'asc' ? -1 : 1;
+    }
+    if (fieldA > fieldB) {
+      return sortOrder === 'asc' ? 1 : -1;
+    }
+    return 0;
+  });
+
+  return data;
+};
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import data from './data/ghibli/ghibli.js';
-import { example } from './data.js';
-console.log(example, data);
+import { sortData } from './data.js';
+
+
+console.log(data);
 
 document.addEventListener("DOMContentLoaded", function() {
   let moviesbutton = document.getElementById("moviesbutton");
@@ -11,9 +13,22 @@ document.addEventListener("DOMContentLoaded", function() {
     home.style.display = "none"; // Oculta el div "home"
     pagemovies.style.display = "grid"; // Muestra el div "pagemovies"
   });
-});
 
-
+  
+  const sortTitle = document.getElementById("sort");
+  sortTitle.addEventListener("change",()=>{
+   const useroption = sortTitle.value;
+   const sortedDataAsc = sortData(data, useroption);
+   console.log(sortedDataAsc);
+  })
+//   // Ejemplo de uso de sortData para ordenar de forma ascendente
+//    const sortedDataAsc = sortData(data, useroption);
+//    console.log(sortedDataAsc);
+ 
+//    // Ejemplo de uso de sortData para ordenar de forma descendente
+//    const sortedDataDesc = sortData(data, 'title', 'desc');
+//    console.log(sortedDataDesc);
+ });
 
 
   
