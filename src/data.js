49edf98f0c9281b1bfc8cfb27c81a35fd1a4401c
Your Name <apmontillal@gmail.com>
2023-07-04 20:54:33 -0500

    cambiototal

--- src/data.js
+++ src/data.js
@@ -3,39 +3,18 @@ export const example = () => {
   return 'example';
 };
 
-export const sort = (data, sortOrder) => {
-  data.sort((a,b)=>{
-    if (sortOrder === 'asc'){
-      return a.title < b.title;
+export const sorted = (data, sortOrder) => {
+  return data.slice().sort((a, b) => {
+    if (sortOrder === 'asc') {
+      return a.title.localeCompare(b.title);
+    } else {
+      return b.title.localeCompare(a.title);
     }
-    else {
-      return b.title > b.title;
-    }
-  })
-};
-
-
-
-export const sortData = (data, sortBy, sortOrder) => {
-    // Verificar si data es un arreglo
-    
-  data.sort((a, b) => {
-    //  método sort() de los arrays y una función de comparación
-    // vamos a convertir los campos a ordenar a mayúsculas para asegurar una comparación correcta
-    const fieldA = a[sortBy].toUpperCase();
-    const fieldB = b[sortBy].toUpperCase();
-
-    if (fieldA < fieldB) {
-      return sortOrder === 'asc' ? -1 : 1;
-    }
-    if (fieldA > fieldB) {
-      return sortOrder === 'asc' ? 1 : -1;
-    }
-    return 0;
   });
-
-  return data;
 };
+
+
+
 
 
 
--- src/main.js
+++ src/main.js
@@ -1,34 +1,64 @@
 import data from './data/ghibli/ghibli.js';
-import { sortData } from './data.js';
+import { sorted, } from './data.js';
+
+const films=data.films;
+
+const pagemovies =document.getElementById("pagemovies");
+
+const showAllCards= (films) => {
+ 
+  for (let i = 0; i < films.length; i++) {
+    let card = document.createElement("div");
+    card.className = "card";
+
+    let title = document.createElement("h2");
+    title.innerText = "Title: " + films[i].title;
+
+    let release_date = document.createElement("p");
+    release_date.innerText = "Date " + films[i].release_date;
+
+    let director = document.createElement("p");
+    director.innerText = "Director: " + films[i].director;
+
+    let producer = document.createElement("p");
+    producer.innerText = "Producer: " + films[i].producer;
+
+    let poster = document.createElement("img");
+    poster.src = films[i].poster;
+
+    card.appendChild(title);
+    card.appendChild(release_date);
+    card.appendChild(director);
+    card.appendChild(producer);
+    card.appendChild(poster);
+
+    pagemovies.appendChild(card);
+  }
+}
 
 
-console.log(data);
+console.log(sorted(films,"asc"));
 
-document.addEventListener("DOMContentLoaded", function() {
-  let moviesbutton = document.getElementById("moviesbutton");
-  let home = document.querySelector(".home");
-  let pagemovies = document.querySelector(".pagemovies");
+//document.addEventListener("DOMContentLoaded", function() {
+  //let moviesbutton = document.getElementById("moviesbutton");
+  //let home = document.querySelector(".home");
+  //let pagemovies = document.querySelector(".pagemovies");
 
-  moviesbutton.addEventListener("click", function() {
-    home.style.display = "none"; // Oculta el div "home"
-    pagemovies.style.display = "grid"; // Muestra el div "pagemovies"
-  });
+  //moviesbutton.addEventListener("click", function() {
+    //home.style.display = "none"; // Oculta el div "home"
+    //pagemovies.style.display = "grid"; // Muestra el div "pagemovies"
+ // });
 
   
   const sortTitle = document.getElementById("sort");
   sortTitle.addEventListener("change",()=>{
-   const useroption = sortTitle.value;
-   const sortedDataAsc = sortData(data, useroption);
+   const userOption = sortTitle.value;
+   const sortedDataAsc = sorted(films, userOption);
+   pagemovies.innerHTML =  "";
+   showAllCards(sortedDataAsc);
    console.log(sortedDataAsc);
   })
-//   // Ejemplo de uso de sortData para ordenar de forma ascendente
-//    const sortedDataAsc = sortData(data, useroption);
-//    console.log(sortedDataAsc);
+
+  showAllCards(films);
+
  
-//    // Ejemplo de uso de sortData para ordenar de forma descendente
-//    const sortedDataDesc = sortData(data, 'title', 'desc');
-//    console.log(sortedDataDesc);
- });
-
-
-  
