2b3c9e5ed87c545fd9ed1102d80e42170138402e
Your Name <apmontillal@gmail.com>
2023-07-07 15:31:18 -0500

    infaddmovies

--- src/main.js
+++ src/main.js
@@ -5,8 +5,7 @@ const films=data.films;
 const pagemoviebig= document.getElementById("pagemoviebig");
 const pagemovies =document.getElementById("pagemovies");
 
-const showAllCards= (films) => {
- 
+const showAllCards = (films) => {
   for (let i = 0; i < films.length; i++) {
     const card = document.createElement("div");
     card.className = "card";
@@ -14,29 +13,61 @@ const showAllCards= (films) => {
     const title = document.createElement("h2");
     title.innerText = "Title: " + films[i].title;
 
-    const release_date = document.createElement("p");
-    release_date.innerText = "Date " + films[i].release_date;
+    const release_date = document.createElement("h1");
+    release_date.innerText = "Date: " + films[i].release_date;
 
-    const director = document.createElement("p");
+    const director = document.createElement("h1");
     director.innerText = "Director: " + films[i].director;
 
-    const producer = document.createElement("p");
+    const producer = document.createElement("h1");
     producer.innerText = "Producer: " + films[i].producer;
 
     const poster = document.createElement("img");
     poster.src = films[i].poster;
 
     card.appendChild(title);
+    card.appendChild(poster);
     card.appendChild(release_date);
     card.appendChild(director);
     card.appendChild(producer);
-    card.appendChild(poster);
+    
+
+    const cardContent = document.createElement("div");
+    cardContent.className = "card-content";
+
+    const description = document.createElement("p");
+    description.innerText = "Description: " + films[i].description;
+
+    const score = document.createElement("h3");
+    score.innerText = "Score: " + films[i].rt_score;
+    
+    
+    const titleCopy = title.cloneNode(true);
+    const release_dateCopy = release_date.cloneNode(true);
+    const directorCopy = director.cloneNode(true);
+    const producerCopy = producer.cloneNode(true);
+    const posterCopy = poster.cloneNode(true);
+
+    cardContent.appendChild(titleCopy);
+    cardContent.appendChild(release_dateCopy);
+    cardContent.appendChild(directorCopy);
+    cardContent.appendChild(producerCopy);
+    cardContent.appendChild(score);
+    cardContent.appendChild(description);
+    cardContent.appendChild(posterCopy);
+
+
+    card.addEventListener("click", () => {
+      card.style.display = "none";
+      pagemovies.style.display="none";
+      cardContent.style.display = "grid";
+    });
 
     pagemovies.appendChild(card);
+    pagemoviebig.appendChild(cardContent);
     pagemoviebig.appendChild(pagemovies);
   }
 };
-
 
   
 const sortTitle = document.getElementById("sort");
@@ -89,6 +120,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const pagemoviesbig = document.getElementById("pagemoviebig"); // Corregido el ID del elemento
   const btnhome = document.getElementById("btnhome");
 
+
   moviesbutton.addEventListener("click", function() {
     home.style.display = "none";//que no se vea la pagina inicial
     pagemoviesbig.style.display = "grid";//que se vea pagemoviebig
@@ -99,22 +131,9 @@ document.addEventListener("DOMContentLoaded", function() {
     home.style.display = "block";//que se vea
     pagemoviesbig.style.display = "none";
   });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
  
+
+});
+ 
+
+
